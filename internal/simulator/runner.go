f51456dbfc73bd9a7263decae3a05a92883256a0
mcordal <mmcordal123@gmail.com>
2026-06-25 15:51:51 +0300

    simulator runner add

--- internal/simulator/runner.go
+++ internal/simulator/runner.go
@@ -1,7 +1,48 @@
 package simulator
 
-import "context"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
 
 func Run(ctx context.Context, cfg Config) error {
-	return nil
+	if cfg.URL == "" {
+		return errors.New("simulator URL is required")
+	}
+
+	if cfg.RPS <= 0 {
+		cfg.RPS = 1
+	}
+
+	if cfg.Duration <= 0 {
+		cfg.Duration = time.Second * 10
+	}
+
+	interval := time.Second / time.Duration(cfg.RPS)
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, cfg.Duration)
+	defer cancel()
+
+	sendCount := 0
+
+	for {
+		select {
+		case <-timeoutCtx.Done():
+			fmt.Printf("simulator finished, sent count: %d\n", sendCount)
+			return nil
+		case <-ticker.C:
+			log := GenerateNormalLog(cfg.Domain)
+
+			err := SendLog(context.Background(), cfg.URL, log)
+			if err != nil {
+				return errors.New("simulator failed to send log: " + err.Error())
+			}
+			sendCount++
+		}
+	}
+
 }
